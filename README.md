cc2e3afee54f466b8a16ccda7bb65d91ea78fa92
chaosmops <hugo.sulger@aol.com>
2025-03-17 14:36:22 +0100

    Update and rename README.md to Store.py
    u alr?

--- README.md
+++
@@ -1,2 +0,0 @@
-# Store
-my nuts kinda hurt
---
+++ Store.py
@@ -0,0 +1,11 @@
+# Store
+import sqlite3
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+@app.route("/")
+
+def home():
+  return render_template("Store.html")
+
+
